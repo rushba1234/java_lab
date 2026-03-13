83a24ee3df8a7d19b98e3e33e210f505418f56df
rushba1234 <rushbanunu@gmail.com>
2026-03-13 10:29:45 +0530

    done

---
+++ search.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class search{
+        public static void main(String args[]){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter array size:");
+                int n = sc.nextInt();
+                int arr[] = new int[n];
+                System.out.println("Enter array elements:");
+                for(int i=0;i<n;i++){
+                        arr[i] = sc.nextInt();
+                }
+
+                System.out.println("Enter the array element to search:");
+                int key = sc.nextInt();
+                int index = -1;
+
+                for(int i=0;i<n;i++){
+                        if(arr[i] == key){
+                                index = i;
+                                break;
+                        }
+                }
+                if(index != -1){
+                        System.out.println("Element found at position: "+index);
+                }else{
+                        System.out.println("Element not found");
+                }
+        }
+}
